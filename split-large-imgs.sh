d279246664d2c7f8242a1011747b36e1ff880fb0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-07-23 22:57:09 +0530

    marble-firmware: Remove existing parts before splitting
    Otherwise we could get a corrupted image on major up/downgrades.

--- split-large-imgs.sh
+++ split-large-imgs.sh
@@ -11,6 +11,7 @@ touch vendorsetup.sh
 chmod +x vendorsetup.sh
 for f in $LARGE_IMGS; do
     echo splitting $f
+    rm -f $f.part*
     split --bytes=20M -d $f $f.part
     echo "cat $REL_PATH/$f.part* > $REL_PATH/$f" >> vendorsetup.sh
     echo $f >> .gitignore
